49bcac1f43a7123963d9a86eb534f2527221a267
Matthew Derocher <mjamesderocher@gmail.com>
2017-06-09 18:57:48 -0400

    Style changes

--- src/App.js
+++ src/App.js
@@ -12,30 +12,36 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div id="fullpage">
+        <main id="fullpage">
           <div className="section intro">
             <img src={logo} className="app-logo" alt="logo" />
+            <h1>Bible School located in Singida, Tanzania</h1>
           </div>
           <div className="section about">
-            <h2>Brief bio</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pulvinar luctus tincidunt. Pellentesque viverra enim quis augue finibus, sit amet sollicitudin mauris posuere. In ac libero fringilla, suscipit lorem in, dapibus ligula. Fusce velit sem, dignissim vel quam vitae, pellentesque pretium dolor. Praesent bibendum imperdiet aliquet. Quisque ut eros dui. Sed egestas odio ac eleifend imperdiet. Etiam suscipit sollicitudin ligula, ut consectetur sapien facilisis in. Morbi a risus vitae ligula gravida dapibus. Ut eget odio egestas, ultricies ipsum id, consequat tellus. Nullam et velit quis nulla cursus volutpat.</p>
-
-            <p>Donec sed aliquet dolor. Nullam augue arcu, mollis vel tempus eleifend, volutpat et velit. Fusce suscipit massa et efficitur sodales. Cras dapibus odio sed tempor fermentum. Donec pellentesque quam metus. Fusce eleifend elit nec nulla auctor pulvinar. Etiam sit amet massa in nibh luctus sagittis. Duis hendrerit sed enim quis hendrerit. In blandit velit et magna lobortis porttitor. Morbi eget nisl in lacus molestie vulputate quis porttitor nulla. Nam ut mauris mi.</p>
-
-            <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec iaculis tincidunt euismod. Praesent a pellentesque dolor, id lobortis justo. Morbi maximus, felis in condimentum laoreet, massa velit scelerisque lorem, ac dictum tortor ligula et augue. Curabitur fermentum, odio id tincidunt sollicitudin, tortor est imperdiet sem, in porttitor ligula odio faucibus felis. Phasellus placerat nisl laoreet, blandit justo sed, vehicula lorem. Suspendisse potenti. Nulla vulputate id nibh ac maximus. Phasellus et nibh et justo semper condimentum id id elit. Mauris luctus eget eros non volutpat. Curabitur vel laoreet quam. Donec vel ultricies odio. Praesent tincidunt urna orci, vitae mollis velit consequat in. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+            <h2>Making disciples out of all people</h2>
+            <p>Bible Exchange School of Ministry is a an independent Bible school located just outside of Singida Town, Tanzania, Africa. More than just teaching knowledge, the School of Ministry's purpose is to equip Christian men and women to become effective ministers of the Gospel. It is our belief that all Christians, and not just those with official ministry positions, can build up other members of the church with their words and deeds. Our goal is to help all students that attend to better reach this potential.</p>
+            <p>Originally named Deliverance Bible Institute of Tanzania, Bible Exchange School of Ministry is a child ministry of <a href="http://deliverance.me">Deliverance Center</a>, an independent church in Maine, USA. The school has been opperating in Singida since August 2015.</p>
           </div>
-          <div className="section">
+          <div className="section slider">
             <div className="slide jacob">
               <img className="avatar" src={jacobFace} alt="Jacob Gariel" />
+              <h3>Jacob Mfandi</h3>
+              <h4>Teacher</h4>
             </div>
             <div className="slide matthew">
-              <img className="avatar" src={matthewFace} alt="Jacob Gariel" />
+              <img className="avatar" src={matthewFace} alt="Matthew Derocher" />
+              <h3>Matthew Derocher</h3>
+              <h4>Teacher</h4>
             </div>
             <div className="slide hosea">
               <img className="avatar" src={hoseaFace} alt="Hosea" />
+              <h3>Hosea</h3>
+              <h4>Teacher</h4>
             </div>
             <div className="slide reuben">
-              <img className="avatar" src={reubenFace} alt="Jacob Gariel" />
+              <img className="avatar" src={reubenFace} alt="Reuben Elijah" />
+              <h3>Reuben Elijah</h3>
+              <h4>Translator</h4>
             </div>
           </div>
           <div className="section courses">
@@ -62,7 +68,7 @@ class App extends Component {
           <div className="section donate">
             <button className="call-to-action">Donate</button>
           </div>
-        </div>
+        </main>
       </div>
     );
   }
